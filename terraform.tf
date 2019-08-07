ac37dd4e489298a405bf5e02755065d31aa15b60
Gunnar Morling <gunnar.morling@googlemail.com>
2019-08-07 14:20:04 +0200

    Upgrading to Fedora 30

--- terraform.tf
+++ terraform.tf
@@ -7,16 +7,7 @@ provider "hcloud" {
 
 resource "hcloud_server" "control" {
   name        = "control"
-  image       = "fedora-28"
-  datacenter  = "nbg1-dc3"
-  server_type = "cx11"
-  keep_disk   = true
-  ssh_keys    = ["key1"]
-}
-
-resource "hcloud_server" "devoxx-ma" {
-  name        = "devoxx-ma"
-  image       = "fedora-28"
+  image       = "fedora-30"
   datacenter  = "nbg1-dc3"
   server_type = "cx11"
   keep_disk   = true
@@ -25,8 +16,4 @@ resource "hcloud_server" "devoxx-ma" {
 
 output "control_public_ip4" {
   value = "${hcloud_server.control.ipv4_address}"
-}
-
-output "devoxx-ma_public_ip4" {
-  value = "${hcloud_server.devoxx-ma.ipv4_address}"
 }
